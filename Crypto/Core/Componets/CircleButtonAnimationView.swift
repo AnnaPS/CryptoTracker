3825d7dc422939916824175ff82775e659989277
Ana Polo <anna-91_@hotmail.com>
2025-09-14 19:33:10 +0200

    first commit

---
+++ Crypto/Core/Componets/CircleButtonAnimationView.swift
@@ -0,0 +1,18 @@
+//
+//  CircleButtonAnimationView.swift
+//  Crypto
+//
+//  Created by Ana Polo on 14/9/25.
+//
+
+import SwiftUI
+
+struct CircleButtonAnimationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircleButtonAnimationView()
+}
